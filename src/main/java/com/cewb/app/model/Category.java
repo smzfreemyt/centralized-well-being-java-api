9b503ef8de0fb29380801cd81d33bbb4d01a5e12
Samuel Amador <samuel.amador@freemight.com>
2021-08-05 11:38:41 +0800

    Add fields for Model Category and Post

--- src/main/java/com/cewb/app/model/Category.java
+++ src/main/java/com/cewb/app/model/Category.java
@@ -2,5 +2,41 @@ package com.cewb.app.model;
 
 public class Category {
 
+    private Long id;
 
+    private String name;
+
+    private String slug;
+
+    public Category() {
+    }
+
+    public Category(String name, String slug) {
+        this.name = name;
+        this.slug = slug;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
 }
--- src/main/java/com/cewb/app/model/Post.java
+++ src/main/java/com/cewb/app/model/Post.java
@@ -1,4 +1,42 @@
 package com.cewb.app.model;
 
 public class Post {
+
+    private Long id;
+
+    private String title;
+
+    private String body;
+
+    public Post() {
+    }
+
+    public Post(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
