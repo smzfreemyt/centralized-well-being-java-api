4d80734e8b62b91353e0e09e8e299e3406245eae
mlph-jandag <john.andag@monstar-lab.com>
2021-08-06 12:33:25 +0800

    added profiles

--- src/main/java/com/cewb/app/controller/UserController.java
+++ src/main/java/com/cewb/app/controller/UserController.java
@@ -1,11 +1,5 @@
 package com.cewb.app.controller;
 
-import org.apache.tomcat.jni.User;
-
 public class UserController {
-	
-	public static void main(String[] args) {
-		com.cewb.app.model.User us = new com.cewb.app.model.User();
-	}
 	
 }
