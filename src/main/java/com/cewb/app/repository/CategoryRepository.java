6ea035486cd3578fc9cc571d58a989d65062b3b4
Rovin Leano <rovin.leano@monstar-lab.com>
2021-08-09 16:11:31 +0800

    Added categories crud

---
+++ src/main/java/com/cewb/app/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.cewb.app.repository;
+
+import com.cewb.app.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
--- src/main/java/com/cewb/app/service/CategoryService.java
+++ src/main/java/com/cewb/app/service/CategoryService.java
@@ -1,4 +1,16 @@
 package com.cewb.app.service;
 
+import com.cewb.app.model.Category;
+import org.springframework.data.domain.Page;
+
 public interface CategoryService {
+
+    Page<Category> findAll(int pageNum);
+
+    Category findById(Long id);
+
+    Category save(Category category);
+
+    Category delete(Long id);
+
 }
