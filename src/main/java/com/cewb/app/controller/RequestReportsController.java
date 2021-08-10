8f84c0b581f2c246f5ab2aca2be08f2d076765e3
mlph-jandag <john.andag@monstar-lab.com>
2021-08-10 11:01:26 +0800

    added updated Company model, updated security config

---
+++ src/main/java/com/cewb/app/controller/RequestReportsController.java
@@ -0,0 +1,12 @@
+package com.cewb.app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/reports")
+public class RequestReportsController {
+	
+	
+	
+}
