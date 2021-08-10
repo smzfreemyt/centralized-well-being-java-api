92cade843fa8e9e6bfcc60ee0073e36f789b148d
mlph-jandag <john.andag@monstar-lab.com>
2021-08-10 15:39:58 +0800

    read functionality for reports, reports collection in postman

--- src/main/java/com/cewb/app/controller/HRRequestReportsController.java
+++ src/main/java/com/cewb/app/controller/HRRequestReportsController.java
@@ -1,16 +1,23 @@
 package com.cewb.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cewb.app.config.RequestStatus;
 import com.cewb.app.model.HRRequest;
 import com.cewb.app.service.HRRequestService;
 
+import lombok.extern.log4j.Log4j2;
+
 @RestController
+@Log4j2
 public class HRRequestReportsController {
 	
 	@Autowired
@@ -19,17 +26,35 @@ public class HRRequestReportsController {
 	// Save reports by users
 	@PostMapping("/reports")
 	public HRRequest createRequest(@RequestBody HRRequest request) {
+		log.info("Create report endpoint");
 		request.setStatus(RequestStatus.PENDING.getDescription());
 		
 		return requestService.save(request);
 	}
 	
+	// Get all reports
+	@GetMapping("/reports")
+	public Page<HRRequest> getReports(@RequestParam(value = "page", defaultValue = "0") int pageNum) {
+		log.info("Get all reports");
+		
+		return requestService.findAll(pageNum);
+	}
+	
+	// Update report status
 	@PutMapping("/reports/status")
 	public HRRequest updateRequest(@RequestBody HRRequest request) {
+		log.info("Update report status endpoint");
+		
 		HRRequest item = requestService.findById(request.getId());
 		item.setStatus(request.getStatus());
 		
 		return requestService.update(item);
 	}
+	
+	@GetMapping("/reports/generate")
+	public ResponseEntity<byte[]> generateReport() {
+		return null;
+	}
+	
 	
 }
