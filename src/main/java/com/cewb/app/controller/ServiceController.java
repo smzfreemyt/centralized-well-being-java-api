1933c0083c0ee31a539fc628ad227816d34af649
John Zidrex Andag <86942707+mlph-jandag@users.noreply.github.com>
2021-08-12 08:20:50 +0800

    added cross origin for service controller, added field on company

--- src/main/java/com/cewb/app/controller/ServiceController.java
+++ src/main/java/com/cewb/app/controller/ServiceController.java
@@ -2,6 +2,7 @@ package com.cewb.app.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import lombok.extern.log4j.Log4j2;
 
 @RestController
 @RequestMapping("/companies/{companyId}")
+@CrossOrigin
 @Log4j2
 public class ServiceController {
 	
