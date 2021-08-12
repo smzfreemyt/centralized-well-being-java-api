88e888959650254a57b27aeb078bcda07f3663e5
Samuel Amador <samuel.amador@freemight.com>
2021-08-12 16:37:37 +0800

    persist data

--- src/main/java/com/cewb/app/MainApp.java
+++ src/main/java/com/cewb/app/MainApp.java
@@ -1,15 +1,24 @@
 package com.cewb.app;
 
+import com.cewb.app.config.ConfigRole;
+import com.cewb.app.model.Role;
+import com.cewb.app.model.User;
 import com.cewb.app.repository.RoleRepository;
 import com.cewb.app.repository.UserRepository;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@Log4j2
+@Configuration
 public class MainApp implements CommandLineRunner {
 
 	@Autowired
@@ -18,11 +27,28 @@ public class MainApp implements CommandLineRunner {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private Environment env;
+
+	private PasswordEncoder passwordEncoder;
+
 	public static void main(String[] args) {
 		SpringApplication.run(MainApp.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		if (this.userRepository.count() < 1) {
+			String pass = env.getProperty("cewbs.admin.password");
+			User user = new User(
+					env.getProperty("cewbs.admin.name"),
+					passwordEncoder.encode(pass),
+					env.getProperty("cewbs.admin.email")
+			);
+			Role role = new Role(ConfigRole.ROLE_ADMIN);
+			user.getRoles().add(role);
+			role.getUsers().add(user);
+			this.userRepository.save(user);
+		}
 	}
 }
