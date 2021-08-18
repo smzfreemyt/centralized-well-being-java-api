406550c2fa1c2d26eb21626abaccff0ee16cfee2
Rovin Leano <rovin.leano@monstar-lab.com>
2021-08-18 09:00:05 +0800

    Fixed controller of post

--- src/main/java/com/cewb/app/model/Category.java
+++ src/main/java/com/cewb/app/model/Category.java
@@ -1,11 +1,13 @@
 package com.cewb.app.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
 
 @Getter
 @Setter
@@ -22,7 +24,35 @@ public class Category {
     @NotEmpty(message = "Name must not be empty")
     private String name;
 
+    @JsonIgnore
+    @OneToMany(mappedBy = "category", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<Post> listOfPosts;
+
     public Category(String name) {
         this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Post> getListOfPosts() {
+        return listOfPosts;
+    }
+
+    public void setListOfPosts(List<Post> listOfPosts) {
+        this.listOfPosts = listOfPosts;
     }
 }
